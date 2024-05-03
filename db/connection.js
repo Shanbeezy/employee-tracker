de5b43042b614654633b599b2c4465d4f16d924d
Shannon James Jr. <jay.are.sj@gmail.com>
2024-05-03 19:40:14 -0400

    fixed connection issue/updated json packaging

--- db/connection.js
+++ db/connection.js
@@ -1,21 +1,20 @@
 const mysql = require("mysql2");
 require('dotenv').config();
 
-const connection = mysql.createConnection(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: "localhost",
-         // Your username
-         user: "root",
-        // Your password
-        password: "",
-        database: "employees"
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: process.env.DB_USER,       // Added username in the environment variable to protect login privacy
+    password: process.env.DB_PASSWORD,  // Added password in the environment variable to protect login privacy
+    database: process.env.DB_NAME        // Added database name in the environment variable to protect database privacy
 });
 
 connection.connect(function (err) {
-  if (err) throw err;
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        return;
+    }
+    // When the connection is successfull, it logs the connection ID which can help for debugging
+    console.log('Connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
